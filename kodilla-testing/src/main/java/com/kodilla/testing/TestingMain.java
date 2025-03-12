be1e19b07e4c2045058d64f1261b69dc960211be
Krzemor <107811815+Krzemor@users.noreply.github.com>
2025-03-12 15:04:37 +0100

    6.2 Added tests for calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator(4, 3);
+        int addResult = calculator.add();
+        int subtractResult = calculator.subtract();
+
+        if (addResult == 7) {
+            System.out.println("Adding test ok");
+        } else {
+            System.out.println("Error in adding");
+        }
+
+        if (subtractResult == 1) {
+            System.out.println("Subtracting test ok");
+        } else {
+            System.out.println("Error in subtracting");
+        }
     }
 }
