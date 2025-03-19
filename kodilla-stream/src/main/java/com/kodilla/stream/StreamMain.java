cc234cc849854a8f20501acb9183f2f2d1c4bf63
Krzemor <107811815+Krzemor@users.noreply.github.com>
2025-03-19 17:48:09 +0100

    7.2 Added immutable forum user

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,30 +1,11 @@
 package com.kodilla.stream;
 
-
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.iterate.NumbersGenerate;
 
 public class StreamMain {
     public static void main(String[] args) {
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-//
-//        System.out.println("Calculating expressions with method references");
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        PoemBeautifier beautifier = new PoemBeautifier();
-        beautifier.beautify("Sample text", text -> "ABC " + text + " ABC");
-        beautifier.beautify("Sample text", text -> text.toUpperCase());
-        beautifier.beautify("Sample text", text ->  "~~~ " + text + " ~~~ " + text + " ~~~");
-        beautifier.beautify("Sample text", text -> new StringBuilder(text).reverse().toString());
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerate.generateEven(20);
     }
 }
