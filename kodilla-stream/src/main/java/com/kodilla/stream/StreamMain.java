1e40ed632fdb95e1fd5d47f3e94e929fde638a7d
Krzemor <107811815+Krzemor@users.noreply.github.com>
2025-03-19 13:43:16 +0100

    7.1 Added text beautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream;
+
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier beautifier = new PoemBeautifier();
+        beautifier.beautify("Sample text", text -> "ABC " + text + " ABC");
+        beautifier.beautify("Sample text", text -> text.toUpperCase());
+        beautifier.beautify("Sample text", text ->  "~~~ " + text + " ~~~ " + text + " ~~~");
+        beautifier.beautify("Sample text", text -> new StringBuilder(text).reverse().toString());
+    }
+}
