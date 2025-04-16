4db80e60f0e65bd8ac6e56aa22fa88cc279ded5a
Krzemor <107811815+Krzemor@users.noreply.github.com>
2025-04-16 14:51:24 +0200

    12.1 Added MovieStore and Factorial v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FactorialRunner.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FactorialRunner.java
@@ -3,9 +3,7 @@ package com.kodilla.good.patterns.challenges;
 public class FactorialRunner {
 
     public static void main(String[] args) {
-        Factorial factorial = new Factorial();
-
-        long result = factorial.factorial(5);
+        long result = Factorial.factorial(5);
 
         System.out.println(result);
     }
