e7e30c8e74d7e26309fccaa804bef88ea906e8b9
Krzemor <107811815+Krzemor@users.noreply.github.com>
2025-05-19 18:11:36 +0200

    16.4 Added NamedQueries

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -1,12 +1,14 @@
 package com.kodilla.hibernate.manytomany.dao;
 
 import com.kodilla.hibernate.manytomany.Company;
-import com.kodilla.hibernate.manytomany.CompanyDao;
 import com.kodilla.hibernate.manytomany.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
@@ -14,6 +16,9 @@ public class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -59,5 +64,47 @@ public class CompanyDaoTestSuite {
         } catch (Exception e) {
             //do nothing
         }
+    }
+
+    @Test
+    void testRetrieveEmployeeByLastname() {
+        //Given
+        Employee employee1 = new Employee("John", "Smith");
+        Employee employee2 = new Employee("Stephanie", "Clarckson");
+        Employee employee3 = new Employee("Linda", "Kovalsky");
+
+        employeeDao.save(employee1);
+        employeeDao.save(employee2);
+        employeeDao.save(employee3);
+
+        //When
+        List<Employee> employees = employeeDao.retrieveEmployeeByName("Kovalsky");
+
+        //Then
+        assertEquals(1, employees.size());
+
+        //Cleanup
+        employeeDao.deleteAll();
+    }
+
+    @Test
+    void testRetrieveCompanyByThreeLetters() {
+        //Given
+        Company company1 = new Company("Software Machine");
+        Company company2 = new Company("Microsoft");
+        Company company3 = new Company("Netflix");
+
+        companyDao.save(company1);
+        companyDao.save(company2);
+        companyDao.save(company3);
+
+        //When
+        List<Company> companies = companyDao.retrieveByFirstThreeLetters("Mic");
+
+        //Then
+        assertEquals(1, companies.size());
+
+        //Cleanup
+        companyDao.deleteAll();
     }
 }
